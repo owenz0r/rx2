ad99ea54354f7dc07a5553eaff51a5ee152c4647
Owen McNally <owen.mcnally@gmail.com>
2014-08-05 23:54:46 +0100

    Fixed alignment of player 2 score.

--- Assets/Scripts/ScoreScript.cs
+++ Assets/Scripts/ScoreScript.cs
@@ -6,6 +6,7 @@ public class ScoreScript : MonoBehaviour {
 	public Vector2 pos = new Vector2( 20, 40 );
 	public Vector2 size = new Vector2( 500, 100 );
 	public Color colour;
+	public bool flip = false;
 	
 	private GUISkin skin;
 	private int score = 0;
@@ -19,7 +20,15 @@ public class ScoreScript : MonoBehaviour {
 	{
 		GUI.skin = skin;
 		GUI.contentColor = new Color( colour.r, colour.g, colour.b );
-		GUI.Label ( new Rect( pos.x, pos.y, size.x, size.y ), score.ToString() );
+		GUIStyle labelStyle = skin.GetStyle( "Label" );
+		if( flip )
+		{
+			labelStyle.alignment = TextAnchor.UpperRight;
+		} else {
+			labelStyle.alignment = TextAnchor.UpperLeft;
+		}
+		GUI.Label ( new Rect( pos.x, pos.y, size.x, size.y ), score.ToString(), labelStyle );
+
 	}
 
 	public void incrementScore()
